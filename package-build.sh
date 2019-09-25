9b73efa17036ecbc6b82c7470ac5c0dce21ab59d
JD <419591321@qq.com>
2019-09-25 11:55:14 +0800

    backup

--- package-build.sh
+++ package-build.sh
@@ -3,39 +3,45 @@
 #workspace=0
 workspace=0
 
+BASEDIR=$PWD
+#用来存放合并的framework
+OUTPUTFOLDER=$BASEDIR/build
+
+#合并后的文件夹名称后缀
+OUTPUTFOLDER_SUFFIX="universal"
+
+#用户自定义的目录，可以帮你拷贝到你想要的目录
+LIB_OUTPUTFOLDER=$BASEDIR/Libs
+
+PROJECT_NAME=$(ls | grep xcodeproj | awk -F.xcodeproj '{print $1}')
+WORKSPACE_NAME=${PROJECT_NAME}.xcworkspace
+YO_SCHEME=${PROJECT_NAME}
+
+TARGET_NAME=${PROJECT_NAME}
+
 #-workspace 是否是workspace
 #选项和参数
 while [ -n "$1" ]
 do
     case "$1" in
         -workspace) workspace=1 ;;
+        -scheme) YO_SCHEME="$2"
+            shift ;;
+        -out) LIB_OUTPUTFOLDER="$2"
+            shift ;;
         --) shift
-    break ;;
+        break ;;
         *) echo "$1 is not an option" ;;
     esac
     shift
 done
 
 
-#用来存放合并的framework
-OUTPUTFOLDER=build
-
-#合并后的文件夹名称后缀
-OUTPUTFOLDER_SUFFIX="universal"
-
-#用户自定义的目录，可以帮你拷贝到你想要的目录
-LIB_OUTPUTFOLDER=Libs
-
-PROJECT_NAME=$(ls | grep xcodeproj | awk -F.xcodeproj '{print $1}')
-WORKSPACE_NAME=${PROJECT_NAME}.xcworkspace
-TARGET_NAME=${PROJECT_NAME}
-
-YO_SCHEME=${PROJECT_NAME}
-
 
 function clean_project {
     echo "\033[33m 清理build目录... \033[0m"
-    rm -rf ${LIB_OUTPUTFOLDER}/${PROJECT_NAME}.framework
+    rm -rf ${LIB_OUTPUTFOLDER}/${YO_SCHEME}.framework
+    rm -rf ${OUTPUTFOLDER}
     xcodebuild clean
 }
 
@@ -71,16 +77,16 @@ function rmkdir_library {
 #拷贝一份头文件
 function cp_library_header {
     configuration=$1
-    cp -R ${OUTPUTFOLDER}/${configuration}-iphonesimulator/${PROJECT_NAME}.framework ${OUTPUTFOLDER}/${configuration}-${OUTPUTFOLDER_SUFFIX}/${PROJECT_NAME}.framework
+    cp -R ${OUTPUTFOLDER}/${configuration}-iphonesimulator/${YO_SCHEME}.framework ${OUTPUTFOLDER}/${configuration}-${OUTPUTFOLDER_SUFFIX}/${YO_SCHEME}.framework
 }
 
 function make_fat_library {
     configuration=$1
-    echo "\033[33m xcrun lipo -create ${OUTPUTFOLDER}/${configuration}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME} ${OUTPUTFOLDER}/${configuration}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME} -output ${OUTPUTFOLDER}/${configuration}-${OUTPUTFOLDER_SUFFIX}/${PROJECT_NAME}.framework/${PROJECT_NAME} \033[0m"
+    echo "\033[33m xcrun lipo -create ${OUTPUTFOLDER}/${configuration}-iphonesimulator/${YO_SCHEME}.framework/${YO_SCHEME} ${OUTPUTFOLDER}/${configuration}-iphoneos/${YO_SCHEME}.framework/${YO_SCHEME} -output ${OUTPUTFOLDER}/${configuration}-${OUTPUTFOLDER_SUFFIX}/${YO_SCHEME}.framework/${YO_SCHEME} \033[0m"
 
-    xcrun lipo -create ${OUTPUTFOLDER}/${configuration}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME} \
-    ${OUTPUTFOLDER}/${configuration}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME} \
-    -output ${OUTPUTFOLDER}/${configuration}-${OUTPUTFOLDER_SUFFIX}/${PROJECT_NAME}.framework/${PROJECT_NAME}
+    xcrun lipo -create ${OUTPUTFOLDER}/${configuration}-iphonesimulator/${YO_SCHEME}.framework/${YO_SCHEME} \
+    ${OUTPUTFOLDER}/${configuration}-iphoneos/${YO_SCHEME}.framework/${YO_SCHEME} \
+    -output ${OUTPUTFOLDER}/${configuration}-${OUTPUTFOLDER_SUFFIX}/${YO_SCHEME}.framework/${YO_SCHEME}
 
     if [ $? -eq 0 ]; then
         echo "\033[33m 👏👏👏build success \033[0m"
@@ -108,7 +114,8 @@ function build_platform {
     make_fat_library ${1}
     
     # 拷贝framewrok到用户的目录
-    ditto ${OUTPUTFOLDER}/$1-${OUTPUTFOLDER_SUFFIX}/${PROJECT_NAME}.framework "${LIB_OUTPUTFOLDER}/${PROJECT_NAME}.framework"
+    echo "\033[33m  ditto ${OUTPUTFOLDER}/$1-${OUTPUTFOLDER_SUFFIX}/${YO_SCHEME}.framework ${LIB_OUTPUTFOLDER}/${YO_SCHEME}.framework \033[0m"
+    ditto ${OUTPUTFOLDER}/$1-${OUTPUTFOLDER_SUFFIX}/${YO_SCHEME}.framework ${LIB_OUTPUTFOLDER}/${YO_SCHEME}.framework
 }
 
 build_platform "Release"
